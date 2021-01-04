ed38393dee99a8565e9843fd4e3e0726ac540112
Matthew Huska, PE <mthuska@gmail.com>
2021-01-04 08:05:22 -0600

    Update doggy to get bored after a little while and redirect.

--- Doggy/wheel-follow-389077.c
+++ Doggy/wheel-follow-389077.c
@@ -2,11 +2,18 @@
 #include "cubelet.h"
 #include <stdint.h>
 
-void setup() {
+int loops;
 
+void setup() {
+  loops = 0;
 }
 
 void loop() {
+    
+    loops += 1;
+    int bored = loops > 500 ? 1 : 0;
+    int redirected = loops > 1000 ? 1 : 0;
+
     //Get the sensor values
     double sensor_1 = (double) get_block_value(379281);
     double sensor_2 = (double) get_block_value(379288);
@@ -25,22 +32,33 @@ void loop() {
     int lower_range = 15;
     
     int value = 0;
-    
-    if (avg > 200) {
-        value += (200 - avg) * 2.5;
-    } else if (avg < 150) {
-        value += (255 - avg);
-    }
-    
-    if (sensor_1 <= lower_range && sensor_2 <= lower_range) {
-      // It sees nothing.
-      value = 0;
-    } else if (diff <= 25 && diff >= -25) {
-      // Center - do nothing
+        
+    if (bored == 0) {
+      if (avg > 200) {
+            value += (200 - avg) * 2.5;
+            loops = 0;
+        } else if (avg < 150) {
+            value += (255 - avg);
+            loops = 0;
+        }
+        
+        if (sensor_1 <= lower_range && sensor_2 <= lower_range) {
+          // It sees nothing.
+          value = slow;
+        } else if (diff <= 25 && diff >= -25) {
+          // Center - do nothing
+        } else {
+        //Go left or right
+          value += diff;
+          loops = 0;
+        } 
+
     } else {
-    //Go left or right
-      value += diff;
-    } 
+      value = -really_slow;
+      if (redirected == 1) {
+        loops = 0;
+      }
+    }
     
     if (value < 0) {
         set_drive_direction(BACKWARD);
@@ -50,7 +68,7 @@ void loop() {
     }
 
 
-    block_value = value > 0 && value < really_slow ? really_slow : value;
+    block_value = value;
     set_actuator_value(block_value);
     
 }
